6506c5f17161c3f08ff1fe03c60e95ace076a0f3
Shawn Tabrizi <shawntabrizi@gmail.com>
2017-06-04 16:32:08 -0700

    initial commit of topic expert, can sign in, read emails, and load them into a DB

---
+++ TopicExpert/TopicExpert/TopicExpert/jsonToDB.py
@@ -0,0 +1,67 @@
+import json
+import sqlite3
+import os
+
+def reset_db():
+    delete_db()
+    create_db()
+
+
+def connect_db():
+    file_dir = os.path.dirname(__file__)
+    filename = os.path.join(file_dir, 'db\mail.db')
+    return sqlite3.connect(filename)
+
+
+def delete_db():
+    conn = connect_db()
+    c = conn.cursor()
+    c.executescript('drop table if exists playerHistory')
+    conn.close()
+
+def create_db():
+    conn = connect_db()
+    c = conn.cursor()
+    #c.executescript('drop table if exists playerHistory')
+    c.executescript('''create table if not exists AADTechTalk(
+                                emailId text,
+                                conversationId text,
+                                senderEmail text,
+                                senderName text,
+                                subject text,
+                                body text,
+                                constraint unique_row unique (emailId,conversationId)
+                                )''')
+    conn.close()
+
+
+def jsonParser( raw_json ):
+    dbobject = []
+    json_obj = raw_json
+    for mail in json_obj['value']:
+        mailobject = []
+        mailobject.append(mail['id'])
+        mailobject.append(mail['conversationId'])
+        mailobject.append(mail['sender']['emailAddress']['address'])
+        mailobject.append(mail['sender']['emailAddress']['name'])
+        mailobject.append(mail['subject'])
+        mailobject.append(mail['body']['content'])
+
+        dbobject.append(mailobject.copy())
+
+    return dbobject
+
+
+
+def jsonToDB( raw_json ):
+    conn = connect_db()
+    c = conn.cursor()
+
+    dbobject = jsonParser(raw_json)
+
+    print("Adding Values")
+
+    c.executemany('insert or ignore into AADTechTalk values (?,?,?,?,?,?)', dbobject)
+
+    conn.commit()
+    conn.close()
---
+++ TopicExpert/TopicExpert/TopicExpert/views.py
@@ -0,0 +1,84 @@
+"""
+Routes and views for the flask application.
+"""
+
+from TopicExpert.jsonToDB import *
+
+from datetime import datetime
+from flask import Flask, redirect, url_for, session, request, jsonify, render_template
+from flask_oauthlib.client import OAuth, OAuthException
+from TopicExpert import app, microsoft
+import json
+
+
+import uuid
+
+@app.route('/')
+def index():
+    return render_template('hello.html')
+
+@app.route('/login', methods = ['POST', 'GET'])
+def login():
+
+    if 'microsoft_token' in session:
+        return redirect(url_for('me'))
+
+    # Generate the guid to only accept initiated logins
+    guid = uuid.uuid4()
+    session['state'] = guid
+
+    return microsoft.authorize(callback=url_for('authorized', _external=True), state=guid)
+
+@app.route('/logout', methods = ['POST', 'GET'])
+def logout():
+	session.pop('microsoft_token', None)
+	session.pop('state', None)
+	return redirect(url_for('index'))
+
+@app.route('/login/authorized')
+def authorized():
+	response = microsoft.authorized_response()
+
+	if response is None:
+		return "Access Denied: Reason=%s\nError=%s" % (
+			response.get('error'), 
+			request.get('error_description')
+		)
+		
+	# Check response for state
+	print("Response: " + str(response))
+	if str(session['state']) != str(request.args['state']):
+		raise Exception('State has been messed with, end authentication')
+		
+	# Okay to store this in a local variable, encrypt if it's going to client
+	# machine or database. Treat as a password. 
+	session['microsoft_token'] = (response['access_token'], '')
+
+	return redirect(url_for('me')) 
+
+@app.route('/mail/<int:page_id>')
+def mail(page_id):
+    top = 100
+    skip = top * page_id
+    messages = microsoft.get('me/MailFolders/AAMkADY3M2VkZjI4LTJjMjEtNGZhMS05MjBiLTU3ZWQyMDFkODc3ZgAuAAAAAADv9UQOpbLPRbfgtgI53-P4AQBAiZS0lTedQIHlewGxaW7RAFZOdABfAAA=/messages?$top={0}&$skip={1}'.format(top,skip))
+
+    with open('data.txt', 'w') as outfile:
+        json.dump(messages.data, outfile)
+    jsonToDB(messages.data)
+    return render_template('mail.html', messages=str(messages.data), next=page_id+1)
+
+@app.route('/me')
+def me():
+    me = microsoft.get('me')
+    return render_template('me.html', me=str(me.data), token=str(session['microsoft_token']))
+
+# If library is having trouble with refresh, uncomment below and implement refresh handler
+# see https://github.com/lepture/flask-oauthlib/issues/160 for instructions on how to do this
+
+# Implements refresh token logic
+# @app.route('/refresh', methods=['POST'])
+# def refresh():
+
+@microsoft.tokengetter
+def get_microsoft_oauth_token():
+    return session.get('microsoft_token')
